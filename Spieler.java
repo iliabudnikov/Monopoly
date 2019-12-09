dac51feb632bbfcfdf4eac44bc0ef3c1021e481d
Michel Macke <michel.macke@ewetel.net>
2019-12-09 14:07:05 +0100

    Einige Getter und Setter für Spieler

--- Spieler.java
+++ Spieler.java
@@ -6,12 +6,52 @@ public class Spieler
     //Die Positon des Spielers auf dem Spielfeld
     private int position;
     //Die Grundstücke die der Spieler gekauft hat. Hier wird die Position des Grundstückes gespeichert. 
-    private Arraylist Grundstücke;
+    private ArrayList<Integer> grundstücke;
     //Jeder Spieler braucht eine Spielernummer, damit man die Spieler unterscheiden kann
-    private int Spielernummer;
+    private int spielernummer;
     
     
     public Spieler()
+    {
+        grundstücke = new ArrayList<Integer>();
+    }
+    public int getGeld()
+    {
+        return geld;
+    }
+    public void setGeld(int Geld)
+    {
+        geld = Geld;
+    }
+    public void addGeld(int Geld)
+    {
+        geld += Geld;
+    }
+    public void subtractGeld(int Geld)
+    {
+        geld -= Geld;
+    }
+    public int getPosition()
+    {
+        return position;
+    }
+    public void setPosition(int Position)
+    {
+        position = Position;
+    }
+    public int[] getGrundstücke()
+    {
+        return grundstücke.toArray();
+    }
+    public void addGrundstück(int Position)
+    {
+        
+    }
+    public void removeGrundstück()
+    {
+        
+    }
+    public int getSpielernummer()
     {
         
     }
