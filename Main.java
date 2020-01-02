0f4791ab2ff62715233a35613b60401838eefe0c
IljaAndrejewitsch <ilyabundikov23@gmail.com>
2020-01-02 21:53:06 +0100

    nichts

--- Main.java
+++ Main.java
@@ -237,22 +237,7 @@ public class Main
 				}
 			}
 		}
-		
-		// Würfeln mit 2 Würfeln. Addieren der gewürfelten Zahlen und so viele Felder vorrücken. Zwei gleiche Zahlen = noch einmal würfeln.
-		// Wenn er sich über Start/Los geht, bekommt ein Spieler 200 Geld
-		
-		// Auf welchem Feld steht ein Spieler? Feldspeziefische Funktionen: Kaufen/Miete, Aktionen
-		// Wenn ein Spieler ein Grundstück nicht kaufen möchte, wird es per Auktion an die Spieler verkauft
-		
-		// Jetzt hat ein Spieler die Möglichkeit Häuser zu bauen, falls er alle Straßen einer Farbe besitzt. Es müssen einheitlich viele Häuser auf allen Straßen einer Farbe stehen. Die kosten für ein Haus variieren je nach Straße. Das 5. Haus auf einer Straße ist ein Hotel. Diese haben einen anderen Preis als Häuser. Man kann nur ein Hotel pro Straße bauen (Nicht pro Farbe, Pro Straße; mit dem Hotel kein Gebäude mehr auf der Straßemöglich)
-		
-		// Der Spieler kann während seines Zuges jederzeit mit anderen Spielern handeln. Er kann unbebaute Grundstücke, Geld und "Komme aus dem Gefängnis Frei" Karten frei mit anderen Spielern Tauschen.
-		// Wenn der Spieler nicht genug Geld hat/ mehr Geld haben will, kann er Häuser zum halben Preis verkaufen. Es müssen weiterhin einheitlich viele Häuser auf allen Straßen einer Farbe stehen.
-		// Der Spieler kann eine Hypothek auf ein unbebautes Grundstück aufnehmen. Wie viel Geld der Spieler für eine Hypothek bekommt variiert nach Grundstück.
-		// Wenn auf ein Grundstück eine Hypothek angemeldet ist, bekommt der Besitzer keine Miete, wenn ein anderer Spieler es betritt. Es kann weiterhin gehandelt werden, die Hypothek bleibt bestehen
-		// Um eine Hypothek aufzulösen, muss der Besitzer das Geld, was er durch die Hypothek bekommen hat +10% zurrückbezahlen.       
-
-}
+	}
 	
 	//Ermöglicht dem Spieler alle möglichen eingaben während des Spieles zu machen
 	// true, wenn Zug beenden, sonst false
